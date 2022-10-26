be7dbd98d22e3e28adfdab6bdbe3f02d66648a22
Tamonica Weathers <tamonica@Tamonicas-MBP.attlocal.net>
2022-10-25 21:20:40 -0400

    added code

--- index.js
+++ index.js
@@ -13,10 +13,21 @@ content.append(dogContent)
 let dogImage = document.createElement('img')
 dogImage.setAttribute('class', 'dog-image')
 dogImage.setAttribute('src', './assets/rizzo.jpg')
-content.append(dogImage)
+dogContent.append(dogImage)
 
 let dogDetails = document.createElement('div')
 dogDetails.setAttribute('class', 'dog-details')
-content.append(dogDetails)
+dogContent.append(dogDetails)
 
+let descriptionHeader = document.createElement('h3')
+descriptionHeader.append('Description');
+dogDetails.append(descriptionHeader)
+
+let paragragh = document.createElement('p')
+paragragh.textContent = 'This gentle dog is aloof toward her owner, and never comes when called.She always acts as though any stranger she meets will harm her, anddislikes other animals'
+dogDetails.append(paragragh); 
+
+let descriptionHeader2 = document.createElement('h3')
+descriptionHeader2.append('Feeding Times:')
+dogDetails.append(descriptionHeader2)
 
